3b4b4e882db98b7d9e5c9755cafae574f9effa77
Aalif Mohammad RS <rulesmashpros@gmail.com>
2025-02-07 00:54:56 +0530

    Update percentage_Temp.py
    changed to directly deleting instead of backup 
    creation in desktop

--- percentage_Temp.py
+++ percentage_Temp.py
@@ -1,21 +1,18 @@
 import os
 import shutil
-class percTemp:
-    def __init__(self,temp_path,username):
-        Temp_1=f"C:\\Users\\{username}\\AppData\\Local\\Temp"
-        file_names=os.listdir(Temp_1)
-        for file in file_names:
-            file_path=os.path.join(Temp_1, file)
-            dest_path=os.path.join(temp_path,file)
-            try:
-                shutil.move(file_path, temp_path)
-            except PermissionError:
-                continue
-            except WindowsError:
-                continue
-            except:
+class perc_temp:
+    def __init__(self):
+        mainfile=os.getenv("TEMP")
+        dst_files=os.listdir(mainfile)
+        for file in dst_files:
+            rmv_file=os.path.join(mainfile,file)
+            if os.path.isfile(rmv_file):
                 try:
-                    os.remove(dest_path)
-                    shutil.move(file_path,temp_path)
+                    os.remove(rmv_file)
                 except:
-                    pass
+                    continue
+            else:
+                try:
+                    shutil.rmtree(rmv_file)
+                except:
+                    continue
