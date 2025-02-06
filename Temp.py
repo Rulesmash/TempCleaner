69b51c038d6202d4db3921689b7bb9004680d93f
Aalif Mohammad RS <rulesmashpros@gmail.com>
2025-02-07 01:04:20 +0530

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -1,21 +1,18 @@
 import os
 import shutil
 class normTemp:
-    def __init__(self,temp_path):
-        Temp_2=r"C:\Windows\Temp"
-        file_names=os.listdir(Temp_2)
-        for file in file_names:    #loops and takes name of all the files in folder one by one
-            file_path=os.path.join(Temp_2, file)    #mixes the filename to the directory inorder to target the file
-            dest_path=os.path.join(temp_path,file)    #mixes the filename to destination name where it will be moved
-            try:
-                shutil.move(file_path, temp_path)
-            except PermissionError:
-                continue    #to skip through files with admin permission
-            except WindowsError:
-                continue    #to skip through files that are currently in use
-            except:
+    def __init__(self):
+        mainfile=r"C:\Windows\Temp"
+        dst_files=os.listdir(mainfile)
+        for file in dst_files:
+            rmv_file=os.path.join(mainfile,file)
+            if os.path.isfile(rmv_file):
                 try:
-                    os.remove(dest_path)    #trying to remove the file/folder from repeating in temporary folder
-                    shutil.move(file_path,temp_path)
+                    os.remove(rmv_file)
                 except:
-                    pass
+                    continue
+            else:
+                try:
+                    shutil.rmtree(rmv_file)
+                except:
+                    continue
