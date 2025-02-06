c403249e4a88c9b1516e147babbecaa3cf1cbeee
Aalif Mohammad RS <rulesmashpros@gmail.com>
2025-02-07 00:56:00 +0530

    Update Deleter.py

--- Deleter.py
+++ Deleter.py
@@ -1,17 +1,12 @@
-from percentage_Temp import percTemp
+from percentage_Temp import perc_temp
 from Temp import normTemp
-import os
-class deleter(): #works by making a folder named Temporary Files and move all the files from temp and %temp% to it (if any file cant be moved due to it being in use, then it produces a copy instead)
+class deleter():
     def __init__(self,condition):
-        username=os.getlogin()    #to take username and use it to navigate through directories with usernames
-        temp_file=f'C:\\Users\\{username}\\Desktop\\Temporary Files'
-        if not os.path.exists(temp_file):
-            os.mkdir(temp_file)    #creates the temporary folder in desktop which can be deleted after the program is finished
         match condition:
             case 1:
-                percTemp(temp_file,username)
+                perc_temp()
             case 2:
-                normTemp(temp_file)
+                normTemp()
             case 3:
-                normTemp(temp_file)
-                percTemp(temp_file,username)
+                normTemp()
+                perc_temp()
